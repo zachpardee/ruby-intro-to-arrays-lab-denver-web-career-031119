f68afa9a2ab74056f6517af37955e17ad3973ed1
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 10:53:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,13 @@ end
 def last_element(array)
   array.last
 end
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+def length_of_arra
