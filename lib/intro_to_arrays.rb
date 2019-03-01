5c6797b2141452908a92c77b968e5c58a98d1124
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 10:52:23 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  array = []
+end
+
+def array_with_two_elements 
+  array_2_elements = [1,2]
+end
+
+def first_element(array)
+  array[0]
+end
+
+def third_elenment(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
